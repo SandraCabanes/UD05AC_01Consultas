4fdaa69c9ce4da45c5123ade257777d95c400a8f
Sandra Cabanes <sandraxtv@gmail.com>
2019-01-25 12:05:24 +0100

    Clase 25-1-19

--- src/app/App.java
+++ src/app/App.java
@@ -29,10 +29,10 @@ public class App {
         //sessionFactory = configuration.buildSessionFactory();
         SessionFactory factory = new Configuration().configure().buildSessionFactory(); 
       
-        //CREAR UNA SESION
+        //CREAR UNA SESIÓN
         Session session=factory.openSession();
         
-        //CREAR CONSULTA
+        //CREAR CONSULTAS
         Query query=session.createQuery("SELECT p FROM Profesor p");
         List<Profesor>profesores=query.list();
         for(Profesor profesor : profesores){
@@ -44,14 +44,53 @@ public class App {
         for(Object[] datos : listDatos){
             System.out.println(datos[0]+" -- "+datos[1]);
         }
-        
-        
+                
         query=session.createQuery("SELECT p.nombre FROM Profesor p");
         List<Object>listNombres=query.list();
         for(Object datos : listNombres){
             System.out.println(datos);
         }
         
+        //Unique result
+        Profesor profesor=(Profesor)session.createQuery("SELECT p FROM Profesor p WHERE id=1001 ").uniqueResult();
+        System.out.println("Profesor con id 1001: "+profesor.getNombre());
+        
+        //Paginación
+        int tamanyoPagina=10;
+        int paginaMostrar=7;
+        
+        query=session.createQuery("SELECT p FROM Profesor p Order By p.id");
+        query.setMaxResults(tamanyoPagina);
+        query.setFirstResult(paginaMostrar*tamanyoPagina);
+        List<Profesor>profesoresPaginacion=query.list();
+        
+        for (Profesor p : profesoresPaginacion) {
+            System.out.println(p.toString());
+        }
+        
+        //Consultas con nombre
+        query=session.getNamedQuery("findAllProfesores");
+        List<Profesor>profesoresNombre=query.list();
+        for (Profesor p : profesoresNombre) {
+            System.out.println(p.toString());
+        }
+        
+        //Cosulta con parámetros por nombre
+        String nombre="ISIDRO";
+        String ape1="CORTINA";
+        String ape2="GARCIA";
+        
+        query=session.createQuery("SELECT p FROM Profesor p WHERE p.nombre=:nombre AND p.ape1=:ape1 AND p.ape2=:ape2");
+        query.setString("nombre", nombre);
+        query.setString("ape1", ape1);
+        query.setString("ape2", ape2);
+        
+        List<Profesor>profesoresParametros=query.list();
+        for (Profesor p : profesoresParametros) {
+            System.out.println(p.toString());
+        }
+        
+        //CERRAR SESIÓN
         session.close();
         factory.close();
         
